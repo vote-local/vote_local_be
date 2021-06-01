90fef54006cf894c44ec86e3b1f9a7427d03c51d
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-06-01 17:55:30 -0600

    add fake data to data base

---
+++ db/migrate/20210601235313_create_dummies.rb
@@ -0,0 +1,7 @@
+class CreateDummies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dummies do |t|
+      t.string :name
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_06_01_235313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dummies", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
