3f98c28e8d6f0e10b798184775e0da2175057669
Wyattwicks <wyattwicks@gmail.com>
2021-06-04 17:21:59 -0600

    Add working representative API call

--- app/services/representative_service.rb
+++ app/services/representative_service.rb
@@ -2,32 +2,26 @@ class RepresentativeService
   class << self
     def conn
       Faraday.new(
-          url: 'https://www.googleapis.com/civicinfo/v2/representatives',
+          url: 'https://www.googleapis.com/civicinfo/v2',
           params: {
-            "key" => "#{ENV["GOOGLE_CIVICS_API_KEY"]}"
+            "key" => "#{ENV['GOOGLE_CIVICS_API_KEY']}"
             })
-      end
+    end
 
     def get_representatives_by_address(address)
-      response = conn.get(
-        # url: "/representatives",
-        :params => {
-          "address" => address,
-          "levels" => ["administrativeArea1", "administrativeArea2"]
-        }
-      )
-
-      # :params => {:color => ['red', 'blue']}
-      # response = conn.get("representatives") do |req|
-      #   req.params['address'] = address
-      #   req.params['levels'] = "administrativeArea1"
-      #   req.params['&levels'] = "administrativeArea2"
-      #   req.params['&levels'] = "locality"
-      #   req.params['&levels'] = "regional"
-      #   req.params['&levels'] = "special"
-      #   req.params['&levels'] = "subLocality1"
-      #   req.params['&levels'] = "subLocality2"
-      # end
+      response = conn.get("representatives") do |req|
+        req.params['address'] = address
+        req.options.params_encoder = Faraday::FlatParamsEncoder
+        req.params['levels'] = [
+          'administrativeArea1',
+          'administrativeArea2',
+          'locality',
+          'regional',
+          'special',
+          'subLocality1',
+          'subLocality2'
+        ]
+      end
       parse_data(response)
     end
 
@@ -36,3 +30,5 @@ class RepresentativeService
     end
   end
 end
+
+
--- spec/services/representative_service_spec.rb
+++ spec/services/representative_service_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe RepresentativeService do
-  it "returns correct structure for get elections query", :vcr do
+  it "returns correct structure for get representatives query based on address", :vcr do
     address = "99 E. Arizona St, Denver CO 80210"
     response = RepresentativeService.get_representatives_by_address(address)
-require "pry"; binding.pry
     expect(response).to be_a(Hash)
     expect(response[:offices]).to be_a(Array)
+    expect(response[:levels]).to_not eq('country')
+
     response[:offices].each do |office|
       expect(office[:name]).to be_a(String)
       expect(office[:officialIndices]).to be_a(Array)
     end
+
     expect(response[:officials]).to be_a(Array)
+
     response[:officials].each do |official|
       expect(official[:name]).to be_a(String)
       expect(official[:party]).to be_a(String)
       expect(official[:urls]).to be_a(Array)
       expect(official[:urls][0]).to be_a(String)
+
       if official[:channels] != nil
         expect(official[:channels]).to be_a(Array)
         # twitter = data[:officials].map do |official|
