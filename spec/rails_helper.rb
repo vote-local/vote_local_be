b32d126b594e7e2c14bf2933d9ed8a3007392291
koshea9 <keegan.oshea9@gmail.com>
2021-06-05 12:23:02 -0600

    removed faraday custom encoder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,7 +71,10 @@ RSpec.configure do |config|
     config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
     config.hook_into :webmock
     config.filter_sensitive_data('<key>') { ENV['GOOGLE_CIVICS_API_KEY'] }
-    config.default_cassette_options = { re_record_interval: 30.days }
+    config.default_cassette_options = {
+      re_record_interval: 30.days,
+      match_requests_on: [:method, :uri, :query]
+     }
     config.configure_rspec_metadata!
   end
 end
