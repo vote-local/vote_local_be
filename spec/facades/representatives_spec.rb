f0118c1a2f3c1830ad96e199377f48eb837f85b6
koshea9 <keegan.oshea9@gmail.com>
2021-06-06 20:07:30 -0600

    add test for representative attributes poro creates after facade

--- spec/facades/representatives_spec.rb
+++ spec/facades/representatives_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe "RepresentativesFacade" do
-  it "returns an array of representative objects", :vcr do
+describe "RepresentativesFacade", :vcr do
+  it "returns an array of representative objects" do
     address = "1331 17th Street, Denver CO 80202"
     representatives = RepresentativesFacade.get_representatives(address)
 
@@ -9,4 +9,24 @@ describe "RepresentativesFacade" do
     expect(representatives.first).to be_an_instance_of(Representative)
   end
 
+  it "returns data to poro to allow it to return readable attributes" do
+    address = "1331 17th Street, Denver CO 80202"
+    representatives = RepresentativesFacade.get_representatives(address)
+
+    expect(representatives.first.name).to eq("Jared Polis")
+    expect(representatives.first.party).to eq("Democratic Party")
+    expect(representatives.first.url).to eq("https://www.colorado.gov/governor/")
+    expect(representatives.first.photo_url).to eq("no photo found")
+    expect(representatives.first.id).to eq(nil)
+    expect(representatives.first.office_name).to eq("Governor of Colorado")
+    expect(representatives.first.twitter_handle).to eq("jaredpolis")
+
+    expect(representatives[14].name).to eq("Timothy M. O'Brien")
+    expect(representatives[14].party).to eq("Nonpartisan")
+    expect(representatives[14].url).to eq("https://denverauditor.org/")
+    expect(representatives[14].photo_url).to eq("no photo found")
+    expect(representatives[14].id).to eq(nil)
+    expect(representatives[14].office_name).to eq("Denver City Auditor")
+    expect(representatives[14].twitter_handle).to eq("not found")
+  end
 end
